5ea18c84e03784915c2a0c18c73b835f8b50691d
Raymond Blok <99034225@mydavinci.nl>
2016-03-07 15:05:41 +0100

    jajajaja i am lorde

--- index.php
+++ index.php
@@ -1,20 +1,53 @@
 <?php
-if(isset($_GET['page']))
+function getContent()
 {
-	$page = $_GET['page'];
-}
-else
-{
-	$page = 'home';
-}
-$db = new mysqli('localhost','root','','doomla');
+	if(isset($_GET['page']))
+	{
+		$page = $_GET['page'];
+	}
+	else
+	{
+		$page = 'home';
+	}
 
+	$db = new mysqli('localhost','root','','doomla');
 
-function getContent($page, $db)
-{
-	if
-		$query = "SELECT content FROM pagecontent WHERE page='$page'";
-		$result = $db -> query($query);
-		$content = $result -> fetch_assoc();
-		echo $content['content'];
+	$query = "SELECT content FROM pagecontent WHERE page='$page'";
+	$result = $db -> query($query);
+	$content = $result -> fetch_assoc();
+
+	if($content == null)
+	{
+		echo "de pagina $page is niet gevonden";
+	}
+	else
+	{
+		return $content['content'];
+	}
 }
+function getMenu()
+{
+	if(isset($_GET['page']))
+	{
+		$page = $_GET['page'];
+	}
+	else
+	{
+		$page = 'home';
+	}
+
+	$db = new mysqli('localhost','root','','doomla');
+
+	$query = "SELECT menuoption FROM pagecontent";
+	$result = $db -> query($query);
+	$menuoptions = $result -> fetch_all(MYSQLI_ASSOC);
+	echo "<ul>";
+	foreach($menuoptions as $menuoption){
+		echo "<li><a href=?page=$page>$menuoption[menuoption]</a></li>";
+	}
+	
+	echo "</ul>";
+
+}
+
+require "templates/template.php";
--- templates/template.php
+++ templates/template.php
@@ -1,16 +1,17 @@
-<?php
-require "index.logic.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>doomla</title>
+	<link rel="stylesheet" type="text/css" href="doomla/templates">
 </head>
 <body>
 	<section>
+		<nav>
+			<?php echo getMenu();?>
+		</nav>
 		<article>
-			<?php echo getContent($_GET['page'], $db); ?>
+			<?php echo getContent();?>
 		</article>
 	</section>
 </body>
